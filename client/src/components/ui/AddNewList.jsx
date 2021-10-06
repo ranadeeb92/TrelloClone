fb51940fcb0381f7d6dbdf38b8c0fb32e45c7ba4
rana deeb <rannoushe92@gmail.com>
2021-10-06 12:01:45 -0700

    added newList frontend
    Co-authored-by: Regina <55225918+rgdonovan@users.noreply.github.com>

--- client/src/components/ui/AddNewList.jsx
+++ client/src/components/ui/AddNewList.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddNewList = () => {
+  const [selected, setSelected] = useState(false);
+
   return (
-    <div id="new-list" className="new-list">
-      <span>Add a list...</span>
+    <div id="new-list" className={`new-list ${selected ? "selected" : ""}`}>
+      <span onClick={() => setSelected(true)}>Add a list...</span>
       <input type="text" placeholder="Add a list..." />
       <div>
         <input type="submit" className="button" value="Save" />
-        <i className="x-icon icon"></i>
+        <i className="x-icon icon" onClick={() => setSelected(false)}></i>
       </div>
     </div>
   );
